a70a6d63f95c7f69f3db64881d04346f563e75f3
jafreis <jfigueira4@gmail.com>
2019-02-12 14:43:17 +0000

    Convert the generated OPID to string

--- routes/kyc.js
+++ routes/kyc.js
@@ -7,6 +7,8 @@ const uniqid = require('uniqid');
 const bigInt = require('big-integer');
 
 router.post('/payment', function(req, res, next) {
+	console.log('body: ', req.body);
+
 	let criteria = {
 		idt: req.body.idt,
 		wa: req.body.wa
@@ -15,12 +17,16 @@ router.post('/payment', function(req, res, next) {
 	conn.findById(criteria, function(err, result) {
 		if (err) return res.status(500).send({ 'data': null, 'message': err });
 
+		console.log('r: ', result);
+
 		let isValid = (result.opid === req.body.opid) ? true : false;
 		res.status(200).send({ 'data': isValid, 'message': null });
 	});
 });
 
 router.post('/', function(req, res, next) {
+	console.log('body: ', req.body);
+
 	let criteria = {
 		idt: req.body.idt,
 		wa: req.body.wa
@@ -29,7 +35,8 @@ router.post('/', function(req, res, next) {
 	let body = JSON.parse(JSON.stringify(criteria));
 	// body.opid = uniqid();
 	let limit = bigInt(2).pow(256);
-	body.opid = bigInt.randBetween(1, limit);
+	body.opid = bigInt.randBetween(1, limit).toString();
+	console.log('opid: ', body.opid);
 
 	conn.addOrUpdateInfo(criteria, body, function(err, result) {
 		if (err) return res.status(500).send({ 'data': null, 'message': err });
