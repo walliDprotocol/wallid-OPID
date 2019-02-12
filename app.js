4977a41b45c4a73368e6ac12472d9e5a823e2a3b
jafreis <jfigueira4@gmail.com>
2019-02-12 14:36:24 +0000

    Added project files

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const http = require('http');
+const fs = require('fs');
+const index = require('./routes/index');
+const kyc = require('./routes/kyc');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// enable cors
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+	res.header("Access-Control-Allow-Credentials", "true");
+
+	if ('OPTIONS' === req.method) {
+		//respond with 200
+		res.status(200);
+		res.json();
+	} else {
+		next();
+	}
+});
+
+app.use('/', index);
+app.use('/api/v1/data', kyc);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: err
+	});
+});
+
+module.exports = app;
+let server = http.createServer(app);
+server.listen(3000);
